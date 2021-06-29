52adb699d85275aaa5c8b893dcf5790e0685b065
keyuquan <ke15391609819>
2021-06-29 18:49:00 +0800

    巨量获取数据

--- jobs/src/main/java/com/analysis/boom/jobs/ocean/dao/AdPlanDataDao.java
+++ jobs/src/main/java/com/analysis/boom/jobs/ocean/dao/AdPlanDataDao.java
@@ -34,18 +34,20 @@ public class AdPlanDataDao {
             // 整理参数
             int page = currentPage;
             String[] fields = new String[]{"cost", "show", "avg_show_cost", "click", "avg_click_cost", "ctr", "convert", "convert_cost", "convert_rate", "deep_convert", "deep_convert_cost", "deep_convert_rate", "attribution_convert", "attribution_convert_cost", "attribution_deep_convert", "attribution_deep_convert_cost", "download_start", "download_start_cost", "download_start_rate", "download_finish", "download_finish_cost", "download_finish_rate", "click_install", "install_finish", "install_finish_cost", "install_finish_rate", "active", "active_cost", "active_rate", "register", "active_register_cost", "active_register_rate"};
-            Map<String, Object> map = new HashMap() {
-                {
-                    put("advertiser_id", s.getAdvertiserId());
-                    put("page", page);
-                    put("page_size", 1000);
-                    put("start_date", startDate);
-                    put("end_date", endDate);
-                    put("group_by", new String[]{"STAT_GROUP_BY_FIELD_ID", "STAT_GROUP_BY_FIELD_STAT_TIME"});
-                    put("time_granularity", "STAT_TIME_GRANULARITY_DAILY");
-                    put("fields", fields);
-                }
-            };
+            Map<String, Object> map = new HashMap();
+            map.put("advertiser_id", s.getAdvertiserId());
+            map.put("page", page);
+            map.put("page_size", 1000);
+            map.put("start_date", startDate);
+            map.put("end_date", endDate);
+            map.put("group_by", new String[]{"STAT_GROUP_BY_FIELD_ID", "STAT_GROUP_BY_FIELD_STAT_TIME"});
+            map.put("time_granularity", "STAT_TIME_GRANULARITY_DAILY");
+            map.put("fields", fields);
+            HashMap filtering = new HashMap() {{
+                //   put("status", "AD_STATUS_ALL");
+                put("ad_ids", new String[]{"1699994552770616"});
+            }};
+            map.put("filtering", filtering);
             // 发送数据请求
             String str = HttpUtils.doBodyGet(ad_plan_data_uri, map, s.getAccessToken());
             // 解析数据
