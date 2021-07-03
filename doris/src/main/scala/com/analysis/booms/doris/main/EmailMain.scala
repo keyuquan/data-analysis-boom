b2d58c94ce7872cc50b2e43a46dd70cd1e120379
keyuquan <ke15391609819>
2021-07-03 16:10:55 +0800

    邮件发送整理

--- doris/src/main/scala/com/analysis/booms/doris/main/EmailMain.scala
+++ doris/src/main/scala/com/analysis/booms/doris/main/EmailMain.scala
@@ -1,16 +1,13 @@
 package com.analysis.booms.doris.main
 
 import com.analysis.boom.common.utils.{DateUtils, DorisDBUtils}
-import com.analysis.booms.doris.utils.{EmailUtils, ExcelUtils}
-import java.util
-import java.util.Map
+import com.analysis.booms.doris.utils.EmailUtils
 
 object EmailMain {
   def main(args: Array[String]): Unit = {
     val endDay = DateUtils.getStartDay(-1)
     val conn = DorisDBUtils.getConnection
     val listConf = DorisDBUtils.queryMap(conn, "select pkg_code,pkg_name,pkg_operator  from  doris_boom.app_pkg_conf   order  by cast(ta_project_id as  int)")
-    val mapDataAll: Map[String, util.List[String]] = new util.HashMap[String, util.List[String]]
     val path = "./report_data/"
     listConf.forEach(map => {
       val pkgName = map.get("pkg_name")
